17185dbca008783a87951029dc402e19369e414e
ajay-panchal-099 <ajaypanchal099@gmail.com>
2025-04-15 23:58:50 +0530

    Binding Port

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def refresh_google_trends_only():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
